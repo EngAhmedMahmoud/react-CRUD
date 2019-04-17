74080139fd62fa68459238848b2c724f76bd319f
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-04-17 20:06:20 +0400

    app-init

---
+++ src/components/courseForm/Form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const form = () => {
+    return (
+        < div >
+            <h3>Form</h3>
+        </div >
+    )
+}
+export default form;
---
+++ src/components/courseList/List.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class List extends Component {
+    render() {
+        return (
+            < div >
+                <h3>Course List</h3>
+            </div >
+        )
+    }
+}
+export default List;
